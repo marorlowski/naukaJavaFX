6e03410b727f257df7c399cc8c46a3cb4883eb2b
Marek Orłowski <morlowski@ittouch.pl>
2018-06-27 08:30:02 +0200

    formatka fxml

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -12,36 +13,14 @@ public class Main extends Application {
     }
 
     public void start(Stage primaryStage) throws Exception {
-        StackPane stackPane = new StackPane();
-        Button button = new Button("Przycisk");
-        //stackPane to jakby tło dekoracja sceny
-        stackPane.getChildren().add(button);
 
-        //to scena - może się zmieniać
-        Scene scene = new Scene(stackPane, 400, 600);
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(this.getClass().getResource("/fxml/StageMain.fxml"));
+        StackPane stackPane = loader.load();
 
-        //stage to deski teatru stage/scene/stackPane i dopiero nasze przyciski itp.
+        Scene scene = new Scene(stackPane);
+
         primaryStage.setScene(scene);
-
-        //wymiary ważniejsze
-        primaryStage.setWidth(200);
-        primaryStage.setHeight(400);
-
-        //blokada zmiany rozmiaru
-        primaryStage.setResizable(false);
-
-        //primaryStage.setFullScreen(true);
-
-        //miejsce uruchomienia na ekranie
-        primaryStage.setX(900);
-        primaryStage.setY(300);
-
-        //Style sceny
-        primaryStage.initStyle(StageStyle.DECORATED);
-
-        //Parametr nieprzezroczystości
-        primaryStage.setOpacity(0.7);
-
         primaryStage.setTitle("JavaFX");
         primaryStage.show();
     }
