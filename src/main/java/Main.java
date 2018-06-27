0a88e0e8d7c2c326ea6ee1b8285cf21cba0a5cc8
Marek Orłowski <morlowski@ittouch.pl>
2018-06-27 07:55:38 +0200

    pierwszy commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
 
 public class Main extends Application {
     public static void main(String[] args) {
@@ -7,6 +12,34 @@ public class Main extends Application {
     }
 
     public void start(Stage primaryStage) throws Exception {
+        StackPane stackPane = new StackPane();
+        Button button = new Button("Przycisk");
+        //stackPane to jakby tło dekoracja sceny
+        stackPane.getChildren().add(button);
+
+        //to scena - może się zmieniać
+        Scene scene = new Scene(stackPane, 400, 600);
+
+        //stage to deski teatru stage/scene/stackPane i dopiero nasze przyciski itp.
+        primaryStage.setScene(scene);
+
+        //wymiary ważniejsze
+        primaryStage.setWidth(200);
+        primaryStage.setHeight(400);
+
+        //blokada zmiany rozmiaru
+        primaryStage.setResizable(false);
+
+        //primaryStage.setFullScreen(true);
+
+        //miejsce uruchomienia na ekranie
+        primaryStage.setX(900);
+        primaryStage.setY(300);
+
+        //Style sceny
+        primaryStage.initStyle(StageStyle.DECORATED);
+
+        primaryStage.setTitle("JavaFX");
         primaryStage.show();
     }
 }
